a072f6a9f1379fd91e11f6c1a6f01e6ad58c4fff
Abdelrahman Elfiky <abdelrahman.elfiky152@gmail.com>
2025-08-09 06:43:48 +0300

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import TaskItem from './TaskItem';
+import React from "react";
+import TaskItem from "./TaskItem";
 
 function TaskList({ tasks, onDelete, onEdit, onToggle }) {
   return (
     <div>
-      {tasks.length === 0 && <p>No tasks available</p>}
-      {tasks.map(task => (
+      {tasks.length === 0 ? <p>No tasks</p> : null}
+      {tasks.map((task) => (
         <TaskItem
           key={task.id}
           task={task}
